b36ce149165ef3255d074f65d5953d79280911d9
Joakim Olovsson <joakim.olovsson@gmail.com>
2024-08-07 00:18:44 +0200

    Add some feats...

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -30,7 +30,8 @@ return {
   	opts = {
   		ensure_installed = {
   			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
+  			"html-lsp", "css-lsp" , "prettier",
+        "rust-analyzer"
   		},
   	},
   },
@@ -40,7 +41,8 @@ return {
   	opts = {
   		ensure_installed = {
   			"vim", "lua", "vimdoc",
-       "html", "css"
+       "html", "css", "puppet",
+       "rust"
   		},
   	},
   },
